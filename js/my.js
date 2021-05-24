5605e9b1bab7b858f1cdfc4f7ecb785cd87f2a7e
Irka Mis <irka.mis.bdg@gmail.com>
2021-05-24 22:41:24 +0200

    Last commit

--- js/my.js
+++ js/my.js
@@ -1,8 +1,23 @@
 // Create variables (fullName, phone, hobbies, email, websiteLink, websiteTitle) after this comment
+
+const fullName = 'Irena Miś';
+const phone = false;
+const hobbies = 'Eating, sleeping, doing nothing';
+const email = 'panda@panda.com';
+const websiteLink = 'http://www.panda.com';
+const websiteTitle = 'Irena Miś Website';
+
 // what type should you use if this variables will not be changed?
+
+console.log(typeof fullName);
+console.log(typeof phone);
+console.log(typeof hobbies);
+console.log(typeof email);
+console.log(typeof websiteLink);
+console.log(typeof websiteTitle);
 
 /**
  *  Remove '//' before render(.... to uncomment function and make it work
  */
 
-//  render(fullName, phone, hobbies, email, websiteLink, websiteTitle);
+render(fullName, phone, hobbies, email, websiteLink, websiteTitle);
